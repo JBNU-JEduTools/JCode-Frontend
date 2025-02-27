175dee440aadda8c4bb151285d5f3124645fcbbd
jsh2256 <jsh2256@naver.com>
2025-02-28 02:19:48 +0900

    style : 코드 의미에 영향을 주지 않는 변경사항

--- src/pages/admin/AdminPage.js
+++ src/pages/admin/AdminPage.js
@@ -4,7 +4,10 @@ import Admin from '../../components/admin/Admin';
 
 const AdminPage = () => {
   return (
-    <Box sx={{ pt: 10 }}>
+    <Box sx={{ 
+      backgroundColor: (theme) => 
+        theme.palette.mode === 'dark' ? '#282A36' : '#FFFFFF'
+    }}>
       <Admin />
     </Box>
   );
