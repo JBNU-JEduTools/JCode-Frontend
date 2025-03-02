500dd0fe089273a80a2162bdc8b1bdcdcd929f35
jsh2256 <jsh2256@naver.com>
2025-03-03 01:06:06 +0900

    feat : 모니터링 페이지 라우트 추가가

--- src/pages/watcher/WatcherPage.js
+++ src/pages/watcher/WatcherPage.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import ClassList from '../../components/watcher/ClassList';
 import ClassDetail from '../../components/watcher/ClassDetail';
 import AssignmentDetail from '../../components/watcher/AssignmentDetail';
+import AssignmentMonitoring from '../../components/watcher/AssignmentMonitoring';
 
 const WatcherPage = () => {
   return (
@@ -10,6 +11,7 @@ const WatcherPage = () => {
       <Route path="/" element={<ClassList />} />
       <Route path="/class/:courseId" element={<ClassDetail />} />
       <Route path="/class/:courseId/assignment/:assignmentId" element={<AssignmentDetail />} />
+      <Route path="/class/:courseId/assignment/:assignmentId/monitoring/:userId" element={<AssignmentMonitoring />} />
     </Routes>
   );
 };
